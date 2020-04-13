95759a01448b0f422f91bd16d9d683ad26394c69
Tom de Geus <tdegeus@users.noreply.github.com>
2020-04-13 17:04:40 +0200

    Code-style update (#2)

--- js/buildGUI.js
+++ js/buildGUI.js
@@ -2,19 +2,21 @@ var freqHandwashingSlider = document.getElementById("frequencyHandWashing");
 var socialDistancingSlider = document.getElementById("socialDistancing");
 var economicActivitySlider = document.getElementById("economicActivity");
 
+// Function to pause the simulation for ms times at every step.
+// TODO: is this the best way to do it?
 
 function sleep(ms) {
-  // need this function to pause the simulation for ms times at every step.
-  // TODO: is this the best way to do it?
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function shuffleArray( array ) {
-    //shuffling the elements of the array.
+// Shuffling the elements of the array.
+
+function shuffleArray(array) {
+
     var size = array.length;
     var rand, tmp;
-    for (var i=0 ; i<size ; i++ ) {
-        rand = Math.floor(Math.random()*(size-i))
+    for (var i = 0; i < size; i++) {
+        rand = Math.floor(Math.random() * (size - i))
         tmp = array[i];
         array[i] = array[rand];
         array[rand] = tmp;
@@ -22,41 +24,53 @@ function shuffleArray( array ) {
     return array;
 }
 
-function generateNSpheres(N , R) {
-    /* Generates N spheres of radius R.
-    When initializing, it is not smart to initialize positions and velocities randomly!
-    Particles might be too close to each other, and this will lead to very large accelerations.
-    current Algorithm: Generate a uniform grid, that is based on the radius, so that spheres of radius R
-    can be put on neighboring gridpoints with at least 2R distance, plus some margin.
-    I populate the gridpoints randomly with N spheres. This leads to a well ordered initial state,
-    which looks strange. TODO: find better way to populate in the beginning */
+// Generates N spheres of radius R.
+// When initializing, it is not smart to initialize positions and velocities randomly!
+// Particles might be too close to each other, and this will lead to very large accelerations.
+//
+// current Algorithm:
+// Generate a uniform grid, that is based on the radius, so that spheres of radius R
+// can be put on neighbouring grid-points with at least 2R distance, plus some margin.
+// I populate the grid-points randomly with N spheres. This leads to a well ordered initial state,
+// which looks strange. TODO: find better way to populate in the beginning
 
-    var r_margin= R * 1.2 
+function generateNSpheres(N, R) {
+
+    var r_margin= R * 1.2
     var D = 2 * R ;
-    var d_margin = 2*R * 1.5 ; //50% margin between 2 objects
-    var elem_on_one_line = parseInt(1/d_margin);
-    var maxIndex = elem_on_one_line **2;
+    var d_margin = 2 * R * 1.5 ; // 50% margin between 2 objects
+    var elem_on_one_line = parseInt(1 / d_margin);
+    var maxIndex = elem_on_one_line ** 2;
     const spheres = new Array();
 
-    if (maxIndex < N) throw new Error("Impossible situation, too many or too big spheres.");
+    if (maxIndex < N) {
+        throw new Error("Impossible situation, too many or too big spheres.");
+    }
 
     // Let's keep a bound on the computational cost of this:
-    if (maxIndex>1000000) throw new Error("Radius is too small");
+    if (maxIndex > 1000000) {
+        throw new Error("Radius is too small");
+    }
 
     occupied = [];
-    for (var i=0 ; i<N ; i++) occupied[i]=true; // reservation of N values for N spheres
-    // fill the rest of the array with false.
-    for (i= N ; i< maxIndex ; i++) occupied[i] = false;
-    // Shuffle array so that occupied[i]=true is distributed randomly
+    // reservation of N values for N spheres
+    for (var i = 0; i < N; i++) {
+        occupied[i] = true;
+    }
+    // fill the rest of the array with false
+    for (i = N; i < maxIndex; i++) {
+        occupied[i] = false;
+    }
+    // Shuffle array so that occupied[i] = true is distributed randomly
     shuffleArray(occupied);
     var count = 0;
-    for (var i=0 ; i<maxIndex ; i++) {
+    for (var i = 0; i < maxIndex; i++) {
         if (occupied[i]) {
             // Random velocity. TODO: take velocity from Boltzmann distribution at set temperature!
-            vx= (1-2*Math.random())*0.3;
-            vy=(1-2*Math.random())*0.3;
-            x = r_margin + d_margin*Math.floor(i/elem_on_one_line);
-            y = r_margin+d_margin*(i%elem_on_one_line);
+            vx = (1 - 2 * Math.random()) * 0.3;
+            vy =(1 - 2 * Math.random()) * 0.3;
+            x = r_margin + d_margin * Math.floor(i / elem_on_one_line);
+            y = r_margin + d_margin * (i % elem_on_one_line);
             sphere = new Sphere(R, 1, x, y, vx, vy, count);
             sphere.setStatus(STATUS_HEALTHY);
             spheres[count] = sphere;
@@ -64,28 +78,32 @@ function generateNSpheres(N , R) {
         }
     }
     // Setting the first sphere as infected
-    spheres[N/2].setStatus(STATUS_INFECTED)
+    spheres[N / 2].setStatus(STATUS_INFECTED)
     return spheres
 }
 
-function clearCanvas(){
-    STATIC_VALUES.CONTEXT.clearRect(STATIC_VALUES.MIN_X_COORD,
-        STATIC_VALUES.MIN_Y_COORD, STATIC_VALUES.MAX_X_COORD, STATIC_VALUES.MAX_Y_COORD);
+function clearCanvas() {
+    STATIC_VALUES.CONTEXT.clearRect(
+        STATIC_VALUES.MIN_X_COORD,
+        STATIC_VALUES.MIN_Y_COORD,
+        STATIC_VALUES.MAX_X_COORD,
+        STATIC_VALUES.MAX_Y_COORD);
 }
 
-function drawSpheres(spheres){
+function drawSpheres(spheres) {
     for (var i = 0; i < spheres.length; i++) {
         spheres[i].Draw(STATIC_VALUES.CONTEXT)
     }
 }
 
-function calculateForces(spheres, epsilon, rm, infection_threshold){
-    /*
-    This function loops over all pairs of particles and calculates the forces on particles based
-    on the LJ parameters epsilon and rm. TODO: more efficient than looping over all pairs? Neighbor lists
-    The infection is propagated in this function since the distance is calculated, and the infection rate (for now)
-    is distance dependant. TODO: improve, it's not great that this is shuffled into this function here
-    */
+// This function loops over all pairs of particles and calculates the forces on particles based
+// on the LJ parameters epsilon and rm.
+// TODO: more efficient than looping over all pairs? Neighbor lists
+// The infection is propagated in this function since the distance is calculated,
+// and the infection rate (for now) is distance dependant.
+// TODO: improve, it's not great that this is shuffled into this function here
+
+function calculateForces(spheres, epsilon, rm, infection_threshold) {
     // Variables for calculation of distance
     var dist_x, dist_y, dist, rm_over_dist;
     // variables for enumeration
@@ -94,53 +112,51 @@ function calculateForces(spheres, epsilon, rm, infection_threshold){
     var force;
     // Setting energies and forces to 0 before start of summation over all pairs:
     // ener refers to the total potential energy
-    var ener=0.0;
-    for (i=0; i<spheres.length; i++){
-        //  copying forces into another array because they are needed for velocity propagation rusing
-        // velocity verlet.
+    var ener = 0.0;
+    for (i = 0; i < spheres.length; i++) {
+        // Copying forces into another array because they are needed for velocity propagation using
+        // velocity Verlet.
         spheres[i].old_fx = spheres[i].fx;
         spheres[i].old_fy = spheres[i].fy;
         spheres[i].fx = 0.0;
         spheres[i].fy = 0.0;
     }
-    for (i=0; i<spheres.length; i++){
-        for (j=i+1; j<spheres.length; j++){
+    for (i = 0; i < spheres.length; i++) {
+        for (j = i + 1; j < spheres.length; j++) {
             dist_x = spheres[i].x - spheres[j].x;
             dist_y = spheres[i].y - spheres[j].y;
-            dist = Math.sqrt(dist_x**2 + dist_y**2);
+            dist = Math.sqrt(dist_x ** 2 + dist_y ** 2);
             rm_over_dist = (rm / dist)
-            ener += epsilon*( (rm / dist)**12 - 2* (rm / dist)**6);
-            force = - 12*epsilon * ( rm**12 / dist**14  - rm**6  / dist**8)
-            //~ force = -12 * A / dist**14 * dist_x + 6 *B / dist**8 * dist_x;
-            spheres[i].fx -= force*dist_x
-            spheres[j].fx += force*dist_x
-            //~ force = -12 * A / dist**14 * dist_y + 6 *B / dist**8 * dist_y;
-            spheres[i].fy -= force*dist_y
-            spheres[j].fy += force*dist_y
-            
+            ener += epsilon * ((rm / dist) ** 12 - 2 * (rm / dist) ** 6);
+            force = - 12 * epsilon * (rm ** 12 / dist ** 14 - rm ** 6 / dist ** 8)
+            spheres[i].fx -= force * dist_x
+            spheres[j].fx += force * dist_x
+            spheres[i].fy -= force * dist_y
+            spheres[j].fy += force * dist_y
+
             /* Misusing the fact that distance has been calculated in order to process infections,
             since that is distance dependant in this model. p_infection ~ e^{-dist/infection_threshold}
             */
-            if ( dist < 3*infection_threshold) {
+            if (dist < 3 * infection_threshold) {
                 if ((spheres[i].status == STATUS_INFECTED) && (spheres[j].status == STATUS_HEALTHY)) {
-                    if (Math.random() < Math.exp(- dist / infection_threshold)){
+                    if (Math.random() < Math.exp(- dist / infection_threshold)) {
                             spheres[j].setStatus(STATUS_INFECTED)
                             console.log('Infecting '+j)
                     }
                 } else if ((spheres[j].status == STATUS_INFECTED) && (spheres[i].status == STATUS_HEALTHY)) {
-                    if (Math.random() < Math.exp(- dist / infection_threshold)){
+                    if (Math.random() < Math.exp(- dist / infection_threshold)) {
                             spheres[i].setStatus(STATUS_INFECTED)
                             console.log('Infecting '+i)
                     }
                 }
             }
-                    
+
         }
     }
     return ener
 }
 
-function propagatePositionsVerlet(spheres, dt){
+function propagatePositionsVerlet(spheres, dt) {
     var i;
     var sphere;
     for (i = 0; i < spheres.length; i++) {
@@ -149,7 +165,7 @@ function propagatePositionsVerlet(spheres, dt){
     }
 }
 
-function propagateVelocitiesVerlet(spheres, dt){
+function propagateVelocitiesVerlet(spheres, dt) {
     var i;
     var sphere;
     for (i = 0; i < spheres.length; i++) {
@@ -172,19 +188,18 @@ function propagateVelocitiesVerlet(spheres, dt){
 }
 
 
-function getKineticEnergy(spheres){
+function getKineticEnergy(spheres) {
     var ekin = 0.0;
-    for (var i=0; i<spheres.length; i++){
-        ekin += 0.5 * spheres[i].mass * (spheres[i].vx**2 +spheres[i].vy**2)
+    for (var i = 0; i < spheres.length; i++) {
+        ekin += 0.5 * spheres[i].mass * (spheres[i].vx ** 2 + spheres[i].vy ** 2)
     }
     return ekin
 }
 
-function applyThermostat(spheres, ekin, target){
-    /* Very basic velocity rescaling. Rescales all velocities uniformly so that new kinetic energy
-    is at a target value.
-    
-     */
+// Very basic velocity rescaling. Rescales all velocities uniformly so that new kinetic energy
+// is at a target value.
+
+function applyThermostat(spheres, ekin, target) {
     var aux = Math.sqrt(target/ekin)
     for (var i = 0; i < spheres.length; i++) {
         spheres[i].vx *= aux
@@ -192,10 +207,10 @@ function applyThermostat(spheres, ekin, target){
     }
 }
 
-function timelineInfected(spheres, p_recovery, p_fatality){
+function timelineInfected(spheres, p_recovery, p_fatality) {
     for (var i = 0; i < spheres.length; i++) {
-        if (spheres[i].status == STATUS_INFECTED){
-            if (Math.random() < p_recovery){
+        if (spheres[i].status == STATUS_INFECTED) {
+            if (Math.random() < p_recovery) {
                 spheres[i].setStatus(STATUS_RECOVERED)
             }
         }
@@ -206,14 +221,12 @@ var STATIC_VALUES = null;
 window.onload = async function()
 {
     var canvas = document.getElementById('my_canvas');
-        if(!canvas)
-        {
+        if(!canvas) {
             alert("Canvas not found.");
             return;
         }
     var context = canvas.getContext('2d');
-        if(!context)
-        {
+        if(!context) {
             alert("Canvas context not found.");
             return;
         }
@@ -232,16 +245,16 @@ window.onload = async function()
 
     // Calculating forces and potential energy in this first step:
     epot = calculateForces(spheres, epsilon, rm)
-    //~ ekin = getKineticEnergy(spheres) 
-    //~ etot = (ekin+epot)         
+    //~ ekin = getKineticEnergy(spheres)
+    //~ etot = (ekin+epot)
     istep = 0;
     while (true) {
         // Getting all the slide values here:
         // This is where the "physics" (or rather psychology) needs to come in!
         // How does social distancing affect infection rates?
-        rm = 0.1 + parseInt(socialDistancingSlider.value)/400; // social distancing slider
+        rm = 0.1 + parseInt(socialDistancingSlider.value) / 400; // social distancing slider
         // How does the handwashing frequency affect infection rates?
-        infection_threshold = 1.0/parseInt(freqHandwashingSlider.value)
+        infection_threshold = 1.0 / parseInt(freqHandwashingSlider.value)
         // Economic activity gives the "temperature" of the simulation!.
         temperature = parseInt(economicActivitySlider.value)
 
@@ -252,16 +265,16 @@ window.onload = async function()
         // propagating velocities based on old velocities&forces and new forces:
         propagateVelocitiesVerlet(spheres, dt)
         // get the kinetic energy from v(t+dt)
-        ekin = getKineticEnergy(spheres) 
-        
+        ekin = getKineticEnergy(spheres)
+
         // applying the thermostat which is parametrized by the economic activity slider:
-        //~ if ( (istep % 10) == 0 ){
+        //~ if ( (istep % 10) == 0 ) {
         // Thermostats needs to be apply every now and then
         applyThermostat(spheres, ekin, temperature)
 
         // etot(t+dt) = ekin(t+dt) + epot(t+dt)
-        etot = (ekin+epot)
-        console.log(istep+': '+ etot + ' ' + ekin + '   '+epot+'   ' +rm)
+        etot = (ekin + epot)
+        console.log(istep + ': ' +  etot  +  ' '  +  ekin  +  '   ' + epot + '   '  + rm)
         timelineInfected(spheres, 0.001, 0.01)
         await sleep(20)
         clearCanvas()
@@ -270,11 +283,15 @@ window.onload = async function()
         if ((istep % 100) == 0) {
             console.log('Checking number of sick')
             count_sick = 0;
-            for (var i=0; i<spheres.length; i++){
-                if (spheres[i].status ==  STATUS_INFECTED) count_sick++;
+            for (var i = 0; i < spheres.length; i++) {
+                if (spheres[i].status ==  STATUS_INFECTED)  {
+                    count_sick++;
+                }
             }
-            console.log(count_sick+' sick individuals')
-            if (count_sick == 0) break // stopping if there's noone infected.
+            console.log(count_sick + ' sick individuals')
+            if (count_sick == 0) {
+                break // stopping if there's noone infected.
+            }
         }
     }
 }
